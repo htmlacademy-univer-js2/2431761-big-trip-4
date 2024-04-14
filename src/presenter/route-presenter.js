83e531151844119fbce536a2d8360ef4723cea84
Темников Сергей <tems101104@gmail.com>
2024-04-14 21:39:24 +0500

    Исправление esLint

--- src/presenter/route-presenter.js
+++ src/presenter/route-presenter.js
@@ -18,53 +18,53 @@ export default class Presenter {
   }
 
   init() {
-    this.#points = [...this.#pointsModel.points];
+    this.#boardPoints = [...this.#pointsModel.points];
     this.#renderBoard();
+  }
 
-    #renderPoint(point) {
-      const escKeyDownHandler = (evt) => {
-        if (evt.key === 'Escape') {
-          evt.preventDefault();
-          replaceFormToPoint();
-          document.removeEventListener('keydown', escKeyDownHandler);
-        }
-      };
-
-      const pointComponent = new PointView({
-        point, onEditClick: () => {
-          replacePointToForm();
-          document.addEventListener('keydown', escKeyDownHandler);
-        }
-      });
-
-      const pointEditComponent = new PointEditView({
-        point,
-        onFormSubmit: () => {
-          replaceFormToPoint();
-          document.removeEventListener('keydown', escKeyDownHandler);
-        }
-      });
-
-      function replacePointToForm() {
-        replace(pointEditComponent, pointComponent);
+  #renderPoint(point) {
+    const escKeyDownHandler = (evt) => {
+      if (evt.key === 'Escape') {
+        evt.preventDefault();
+        replaceFormToPoint();
+        document.removeEventListener('keydown', escKeyDownHandler);
       }
-
-      function replaceFormToPoint() {
-        replace(pointComponent, pointEditComponent);
-      }
-
-      render(pointComponent, this.tripPointsComponent.element);
     };
 
-    #renderBoard() {
-      render(new SortView(), this.#container);
-      render(this.tripPointsComponent, this.#container);
-
-      this.#boardPoints.forEach((point) => this.#renderPoint(point));
-
-      if (this.#boardPoints.length === 0) {
-        render(new NoPointsView, this.tripPointsComponent.element);
+    const pointComponent = new PointView({
+      point, onEditClick: () => {
+        replacePointToForm();
+        document.addEventListener('keydown', escKeyDownHandler);
       }
+    });
+
+    const pointEditComponent = new PointEditView({
+      point,
+      onFormSubmit: () => {
+        replaceFormToPoint();
+        document.removeEventListener('keydown', escKeyDownHandler);
+      }
+    });
+
+    function replacePointToForm() {
+      replace(pointEditComponent, pointComponent);
+    }
+
+    function replaceFormToPoint() {
+      replace(pointComponent, pointEditComponent);
+    }
+
+    render(pointComponent, this.tripPointsComponent.element);
   }
-}
+
+  #renderBoard() {
+    render(new SortView(), this.#container);
+    render(this.tripPointsComponent, this.#container);
+
+    this.#boardPoints.forEach((point) => this.#renderPoint(point));
+
+    if (this.#boardPoints.length === 0) {
+      render(new NoPointsView, this.tripPointsComponent.element);
+    }
+  }
 }
