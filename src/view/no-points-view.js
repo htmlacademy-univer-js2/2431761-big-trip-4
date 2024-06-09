028070e1a7af4a2a8eea6abae48be69b25a6eb01
Темников Сергей <tems101104@gmail.com>
2024-06-09 20:12:45 +0500

    Удаление лишних файлов

--- src/view/no-points-view.js
+++
@@ -1,9 +0,0 @@
-import AbstractView from '../framework/view/abstract-view.js';
-
-const createNoPointsTemplate = () => '<p class="trip-events__msg">Click New Event to create your first point</p>';
-
-export default class NoPointsView extends AbstractView {
-  get template() {
-    return createNoPointsTemplate();
-  }
-}
